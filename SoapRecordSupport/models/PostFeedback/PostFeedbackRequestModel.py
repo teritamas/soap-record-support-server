0f90900f3d4bbf6ef5a470a2bdaf7b11aa89cf6b
sey323 <sey323_nm@yahoo.co.jp>
2022-10-16 00:19:34 +0900

    主観評価と客観評価を出せるように修正

--- SoapRecordSupport/models/PostFeedback/PostFeedbackRequestModel.py
+++ SoapRecordSupport/models/PostFeedback/PostFeedbackRequestModel.py
@@ -6,8 +6,8 @@ class PostFeedbackRequestModel(BaseModel):
     department: str = Field(..., title="何科か")
     sex: str = Field(..., title="性別")
     age: int = Field(..., title="年齢")
-    subjective: list[str] = Field(..., title="主観的情報")
-    objective: list[str] = Field(..., title="客観的情報")
+    subjective: str = Field(..., title="主観的情報")
+    objective: str = Field(..., title="客観的情報")
     assessment: str = Field(..., title="評価コメント")
     plan: str = Field(..., title="計画")
     
