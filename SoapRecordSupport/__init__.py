e3c323a993547d3e85dc2f451d0b09d9027a2035
sey323 <sey323_nm@yahoo.co.jp>
2022-10-15 22:54:39 +0900

    corsの設定を追加

--- SoapRecordSupport/__init__.py
+++ SoapRecordSupport/__init__.py
@@ -3,9 +3,20 @@ import logging
 logging.basicConfig(format="[%(levelname)s] %(asctime)s %(message)s", level=logging.INFO)
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from SoapRecordSupport.router import router
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
