7da6c0dd7a58e4dbeb8f5b55f2a8d88c9d73141b
sey323 <sey323_nm@yahoo.co.jp>
2022-10-15 21:01:13 +0900

    feedback_messageの取得完了

--- config.py
+++ config.py
@@ -9,3 +9,6 @@ http_port = int(environ.get("HTTP_PORT", '8010'))
 
 line_channel_access_token = environ.get("LINE_CHANNEL_ACCESS_TOKEN", '')
 line_channel_secret = environ.get("LINE_CHANNEL_SECRET", '')
+
+cred_path = environ.get("CRED_PATH", '')
+firebase_database_url = environ.get("FIREBASE_DATABASE_URL", '')
