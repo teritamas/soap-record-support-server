decc3523cf0d6bc5b4a847bdb7ec0692262902d8
Ichirou.S <sey323@users.noreply.github.com>
2022-10-16 00:21:31 +0900

    CotohaAPIと連携してSOを計算できるように変更 (#6)

--- SoapRecordSupport/models/PostEvaluate/PostEvaluateRequestModel.py
+++ SoapRecordSupport/models/PostEvaluate/PostEvaluateRequestModel.py
@@ -6,6 +6,6 @@ class PostEvaluateRequestModel(BaseModel):
     department: str = Field(..., title="何科か")
     sex: str = Field(..., title="性別")
     age: int = Field(..., title="年齢")
-    subjective: list[str] = Field(..., title="主観的情報")
-    objective: list[str] = Field(..., title="客観的情報")
+    subjective: str = Field(..., title="主観的情報")
+    objective: str = Field(..., title="客観的情報")
     
