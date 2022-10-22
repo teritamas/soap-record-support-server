3fec689caa777989a557f4f31336171bb65c8af2
sey323 <sey323_nm@yahoo.co.jp>
2022-10-23 00:41:38 +0900

    外部APIリクエストするクラスをスネークケースに変更

--- SoapRecordSupport/services/Users.py
+++ SoapRecordSupport/services/Users.py
@@ -1,13 +1,13 @@
 from SoapRecordSupport.models.LineLogin.UserInfoResponse import \
     UserInfoResponse
-from SoapRecordSupport.services import fb
+from SoapRecordSupport.services import fire_base
 
 
 def set_or_get_user(group_id: str, user_id: str, name:str, code: str) -> UserInfoResponse:
     # ユーザが存在していたらそのユーザを返す
-    user = fb.find_group_user(group_id, user_id)
+    user = fire_base.find_group_user(group_id, user_id)
     if not user: # 存在していなければ追加する
-        user = fb.set_group_user(group_id, user_id, name, code)
+        user = fire_base.set_group_user(group_id, user_id, name, code)
 
     return UserInfoResponse(
         user_id=user_id,
--- SoapRecordSupport/services/__init__.py
+++ SoapRecordSupport/services/__init__.py
@@ -3,13 +3,13 @@ from linebot import LineBotApi, WebhookHandler
 from SoapRecordSupport.facade.CotohaFacade import CotohaFacade
 from SoapRecordSupport.facade.Firebase import Firebase
 
-fb = Firebase(
+fire_base = Firebase(
     config.cred_path, 
     config.firebase_database_url,
     "feedback_comments"
 )
 
-ch = CotohaFacade(
+cotoha_facade = CotohaFacade(
     client_id=config.cotoha_client_id,
     client_secret=config.cotoha_client_secret
 )
